a525aa944a54c8c979710e659d9a706aced82eac
Philip Tolton <mrtolton@gmail.com>
2014-03-06 14:33:29 -0500

    push

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Foodcloud::Application.routes.draw do
   devise_scope :user do
     get '/registration/confirm' => 'registration#confirm'
     get '/app/registration/confirm' => 'registration#confirm'
+    match '/app/registration/confirm' => 'registration#confirm', :via => :get
   end  
 
   resources :administration do
