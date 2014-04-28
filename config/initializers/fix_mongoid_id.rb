af09a7b7720abba9fc7b61a85d0e945070520ee9
Philip Tolton <mrtolton@gmail.com>
2014-04-28 10:46:45 -0400

    tweaks

--- config/initializers/fix_mongoid_id.rb
+++ config/initializers/fix_mongoid_id.rb
@@ -12,6 +12,7 @@ end
 module Moped
   class Connection
     def connect
+      Rails.logger.warn "MONGOID BEFORE: host: #{host.to_s} port: #{port.to_s} timeout: #{timeout.to_s}\noptions: #{options.to_s}"
       if host.blank?
         Rails.logger.warn "MONGOID WARNING: host: #{host.to_s} port: #{port.to_s} timeout: #{timeout.to_s}\noptions: #{options.to_s}"
         host = "10.128.224.88"
