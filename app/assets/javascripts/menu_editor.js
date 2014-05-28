a2381626fba155e1c7a32df7ece324e43f6d3642
Philip Tolton <mrtolton@gmail.com>
2014-05-27 21:11:32 -0400

    fix image loading live after cache implementation

--- app/assets/javascripts/menu_editor.js
+++ app/assets/javascripts/menu_editor.js
@@ -1124,6 +1124,7 @@ function MenuViewModel() {
         self.currentOption(null);
         self.currentDishOptions(null);
         self.showOptionsList(false);
+        self.showSizesList(false);
     };
 
     self.copyOptionIntoDish = function(item){
