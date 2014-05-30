274786615f130addc769457b7f0da57231dd1d7f
Philip Tolton <mrtolton@gmail.com>
2014-05-29 22:16:41 -0400

    menu integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Foodcloud::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
