921431dc1f0cbd916ca162f056fd1f66ab076c2c
Philip Tolton <mrtolton@gmail.com>
2014-06-15 13:10:51 -0400

    disable compress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Foodcloud::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
-  config.assets = false
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
