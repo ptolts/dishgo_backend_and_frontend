2fc330cf0179f39599dd7550ccfa8b84824c1c14
Philip Tolton <mrtolton@gmail.com>
2014-06-15 12:25:15 -0400

    disable compress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Foodcloud::Application.configure do
   config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
