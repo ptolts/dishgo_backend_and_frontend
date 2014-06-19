0716ab85bd08b55f68291bf2c6752363e66fa59d
Philip Tolton <mrtolton@gmail.com>
2014-06-19 15:44:20 -0400

    fb menu

--- app/assets/javascripts/menu_editor.js
+++ app/assets/javascripts/menu_editor.js
@@ -34,8 +34,8 @@ ko.bindingHandlers.fbMaxHeight = {
             var maxHeight = 0;
             $(element).children("."+value).each(function(index,elem){
                 var elem = $(elem);
-                if(elem.height() > maxHeight)
-                    maxHeight = elem.height();
+                if(elem.outerHeight() > maxHeight)
+                    maxHeight = elem.outerHeight();
             });
             console.log(maxHeight);
             $(element).children("."+value).css('min-height',maxHeight+'px');
