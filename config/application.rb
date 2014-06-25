6fe33fdfded5829a561010b62335a258946c5ef1
Philip Tolton <mrtolton@gmail.com>
2014-06-25 09:50:23 -0400

    gallery height fix

--- config/application.rb
+++ config/application.rb
@@ -61,27 +61,12 @@ module Foodcloud
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    # config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.compile = true
-
-    config.assets.precompile += [   'menu_editor.js',
-                                    'restaurant.js',
-                                    'menu-builder.css',
-                                    'codemirror.js',
-                                    'codemirror.css',
-                                    'online_site.css',
-                                    'strictly_menu.css',
-                                    'website.css',
-                                    'picker.js',
-                                    'wysiwyg.js',
-                                    'signin.css',
-                                    'onlinesite_javascript.js',
-                                    'loading_widget.css',
-                                ]
+    # config.assets.compile = true
 
     config.exceptions_app = self.routes
 
