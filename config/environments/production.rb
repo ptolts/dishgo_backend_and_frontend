1dd7c7ece77e55869a769eb15ab468ef33719eb1
Philip Tolton <mrtolton@gmail.com>
2014-06-25 09:54:20 -0400

    gallery height fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,10 @@ Foodcloud::Application.configure do
                               ]
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
     class NoCompression
         def compress(string)
