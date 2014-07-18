8f609e0795279a89d5dbafd3c6a3a0af7684bbe1
Philip Tolton <mrtolton@gmail.com>
2014-07-18 18:06:21 -0400

    hold onto your butts, we're offloading work!

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 git pull
 RAILS_ENV=production rake assets:precompile
 sudo service nginx restart
-RAILS_ENV=production script/delayed_job stop
-RAILS_ENV=production script/delayed_job start
+RAILS_ENV=production bin/delayed_job stop
+RAILS_ENV=production bin/delayed_job start
 tail -f log/production.log
