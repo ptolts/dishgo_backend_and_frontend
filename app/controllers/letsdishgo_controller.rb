964c64cf5ffb5f83b7dc0930e065239bf3256d14
Philip Tolton <mrtolton@gmail.com>
2014-07-18 11:06:07 -0400

    restaurant name fix

--- app/controllers/letsdishgo_controller.rb
+++ app/controllers/letsdishgo_controller.rb
@@ -28,7 +28,7 @@ class LetsdishgoController < ApplicationController
 		user.skip_confirmation!
 		user.skip_confirmation_notification!
 		user.save!(:validate => false)
-		restaurant = Restaurant.create(name:"Restaurant Name")
+		restaurant = Restaurant.create
 		user.owns_restaurants = restaurant
 		user.save
 		restaurant.reload
