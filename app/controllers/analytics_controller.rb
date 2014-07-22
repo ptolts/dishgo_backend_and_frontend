c42e3f17841373aa4e89e27b7517fa378f75a6d5
Philip Tolton <mrtolton@gmail.com>
2014-07-22 09:57:27 -0400

    better analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,10 +3,24 @@ class AnalyticsController < ApplicationController
   
   def day_data
     now = DateTime.now 
+    now = Time.new(now.year,now.month,now.day,now.hour).getutc
     before = now - 1.day
-    page_views = PageViewHour.where(:created_at.lte => now, :created_at.gt => before, restaurant_id: params[:restaurant_id])
-    labels = page_views.collect{|e| e.created_at.strftime("%H:00") }
-    data = page_views.collect{|e| e.count }
+    before = Time.new(before.year,before.month,before.day,before.hour).getutc
+    result_hash = {}
+    labels = []
+    label_time = before.dup
+    while label_time != now
+      puts label_time
+      time = label_time.getlocal.strftime("%H:00")
+      result_hash[time] = 0
+      labels << time
+      label_time = label_time + 1.hour
+    end
+    page_views = PageViewHour.where(:created_at.lte => now, :created_at.gte => before, restaurant_id: params[:restaurant_id])
+    page_views.each do |e|
+      result_hash[e.created_at.getlocal.strftime("%H:00")] = e.count
+    end
+    data = result_hash.collect{|e,g| g}
     data_set = {
       labels: labels,
       data: data,
@@ -16,10 +30,23 @@ class AnalyticsController < ApplicationController
 
   def week_data
     now = DateTime.now 
+    now = Time.new(now.year,now.month,now.day).getutc
     before = now - 7.day
-    page_views = PageViewDay.where(:created_at.lte => now, :created_at.gt => before, restaurant_id: params[:restaurant_id])
-    labels = page_views.collect{|e| e.created_at.strftime("%d") }
-    data = page_views.collect{|e| e.count }
+    before = Time.new(before.year,before.month,before.day).getutc
+    result_hash = {}
+    labels = []
+    label_time = before.dup
+    while label_time != now
+      time = label_time.getlocal.strftime("%A")
+      result_hash[time] = 0
+      labels << time
+      label_time = label_time + 1.day
+    end
+    page_views = PageViewDay.where(:created_at.lte => now, :created_at.gte => before, restaurant_id: params[:restaurant_id])
+    page_views.each do |e|
+      result_hash[e.created_at.getlocal.strftime("%A")] = e.count
+    end
+    data = result_hash.collect{|e,g| g}
     data_set = {
       labels: labels,
       data: data,
