0db41ddf9bb7a22e7b63dc5eb88c3c742a2e1507
Philip Tolton <mrtolton@gmail.com>
2014-08-07 13:55:39 -0400

    dishcoins

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,7 @@
 class Rating
   include Mongoid::Document
   field :rating, type: Integer, default: 0
+  field :review, type: String
   belongs_to :user, class_name: "User", index: true
   belongs_to :dish, class_name: "Dish", index: true
   belongs_to :restaurant, class_name: "Restaurant", index: true
