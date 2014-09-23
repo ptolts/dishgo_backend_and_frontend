b617a955d8360f6ef85bb398bb6cd6b615a017c7
Philip Tolton <mrtolton@gmail.com>
2014-09-23 09:24:56 -0400

    hold on to your you know what

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -64,7 +64,7 @@ class NetworkController < ApplicationController
     #   coords = [result.coordinates[1],result.coordinates[0]]
     #   restaurants = restaurants.where(:locs => { "$near" => { "$geometry" => { "type" => "Point", :coordinates => coords }, "$maxDistance" => 100000}})
     # end
-    search_term = params[:restaurant_search_term].gsub(/[^[:alnum:]]/,'.').gsub(/s\b/,'.?s').gsub(/áéíóú/i,'.')
+    search_term = params[:restaurant_search_term].gsub(/[^[:alnum:]]/,'.').gsub(/s\b/,'.?s').gsub(/áéíóúâçûêô/i,'.')
     regex = /#{search_term}/i
     restaurants = restaurants.where(name:regex)
     count = restaurants.count
@@ -79,10 +79,10 @@ class NetworkController < ApplicationController
     #   restaurant_ids = Restaurant.where(:locs => { "$near" => { "$geometry" => { "type" => "Point", :coordinates => coords }, "$maxDistance" => 100000}}).only(:id).collect{|e| e.id}
     #   dishes = dishes.where(restaurant_id:restaurant_ids)
     # end
-    search_term = params[:dish_search_term].gsub(/[^[:alnum:]]/,'.').gsub(/s\b/,'.?s').gsub(/áéíóú/i,'.')
+    search_term = params[:dish_search_term].gsub(/[^[:alnum:]]/,'.').gsub(/s\b/,'.?s').gsub(/áéíóúâçûêô/i,'.')
     regex = /#{search_term}/i
     dishes = dishes.where(search_terms:regex)
-    if restaurant_name = params[:restaurant_search_term].to_s.gsub(/s\b/,'.?s').gsub(/áéíóú/i,'.')
+    if restaurant_name = params[:restaurant_search_term].to_s.gsub(/s\b/,'.?s').gsub(/áéíóúâçûêô/i,'.')
       resto_regex = /#{restaurant_name}/i
       dishes = dishes.where(restaurant_name:resto_regex)
     end
