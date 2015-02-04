ba033e662728845a5a9c95327b03875693d8af98
Philip Tolton <mrtolton@gmail.com>
2015-02-04 11:58:41 -0500

    missed an api key

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Foodcloud::Application.config.secret_token = '10c99d90eddede69772fef433cf72f67fb9fb095fe33cff33707fe832d9dd0aeb399cef071dfb1f7a5d71e50c44ff654c29c240a3d30cf8390f050a038a20c0d'
